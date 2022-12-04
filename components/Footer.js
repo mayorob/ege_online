2ec7a0a8f6a516aef7f162a170d0abb44656a8c0
mayorob <egormayor@icloud.com>
2022-12-05 00:45:27 +0300

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import {SlSocialVkontakte} from 'react-icons/sl'
 const Footer = ({ children, keywords }) => {
   return (
     <>
-      <div className=" gap-3 py-4 px-72 flex text-3xl justify-center text-white font-custom bg-[#17212B]">
+      <div className=" xs:gap-3 py-4 px-72 flex text-3xl justify-center text-white font-custom bg-[#17212B]">
         <Link
               href='https://vk.com'
             >
--- components/MainContainer.js
+++ components/MainContainer.js
@@ -10,7 +10,7 @@ const MainContainer = ({ children, keywords }) => {
       </Head>
       <div className="py-4 px-72 flex justify-between text-white font-custom bg-[#242F3D]">
         <h1 className="py-1 text-xl">EGE.ONLINE</h1>
-        <ul className=" justify-end flex gap-4">
+        <ul className=" hidden xl:flex justify-end gap-4">
         <Link
               href='/russian'
               class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
@@ -30,7 +30,24 @@ const MainContainer = ({ children, keywords }) => {
               Обществознание
             </Link>
         </ul>
+        <button class="inline-block xl:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
       </div>
+      
       <div>{children}</div>
     </>
   );
